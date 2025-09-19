6c5a7237e17d75ac898c32f5e0e44c9a1db5c9af
loteim <wakt7568@naver.com>
2025-09-19 16:59:09 +0900

    equipmentDetail 작성중
    equipmentDetailController 주석시 작동 가능

--- mes-api/src/main/java/com/globalmed/mes/mes_api/employee/repository/EmployeeRepo.java
+++ mes-api/src/main/java/com/globalmed/mes/mes_api/employee/repository/EmployeeRepo.java
@@ -27,4 +27,6 @@ public interface EmployeeRepo extends JpaRepository<EmployeeEntity, String> {
     """)
     List<Object[]> findRawAssignments(@Param("name") String name);
 
+    List<EmployeeEntity> findShiftWorkers(List<String> employeeIds);
+
 }
