aad8bc1367463f5fd683e91ceb5042962373b919
JANGBEOMSU <qja1397@gmail.com>
2025-09-24 20:52:26 +0900

    25.09.24 설비화면 조정

--- mes_app/src/components/layout/AppLayout.tsx
+++ mes_app/src/components/layout/AppLayout.tsx
@@ -1,19 +1,15 @@
 import { Outlet } from "react-router-dom";
 import LeftSidebar from "./leftsidebar";
-// import RightSidebar from "./RightSidebar";
 import ToastHost from "../common/ToastHost";
 import { useState } from "react";
 
 export default function AppLayout() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false); // 왼쪽 사이드바 기본 열림
-  const [isRightSidebarOpen, setIsRightSidebarOpen] = useState(false);
 
   const toggleSidebar = () => setIsSidebarOpen(!isSidebarOpen);
-  const toggleRightSidebar = () => setIsRightSidebarOpen(!isRightSidebarOpen);
 
   return (
     <div className="flex min-h-screen bg-blue-50 relative">
-       {/* <div className="flex min-h-screen bg-blue-50 relative"> */}
       {/* 왼쪽 사이드바 */}
       <LeftSidebar
         isOpen={isSidebarOpen}
@@ -23,7 +19,6 @@ export default function AppLayout() {
       {/* 메인 콘텐츠 영역 */}
       <div className="flex-1 flex flex-col overflow-hidden">
         <header className="flex items-center justify-between p-4 bg-blue-100 border-b shadow">
-          {/* <header className="flex items-center justify-between p-4 bg-blue-100 border-b shadow">*/}
           <button
             onClick={toggleSidebar}
             className="p-4 rounded hover:bg-gray-300 , py-2"
@@ -32,12 +27,6 @@ export default function AppLayout() {
           </button>
           <div className="text-xl font-bold">GlobalMed</div>
           <div className="flex items-center space-x-4">
-            {/* <button
-              onClick={toggleRightSidebar}
-              className="p-2 rounded hover:bg-gray-400"
-            >
-              ⚙
-            </button> */}
           </div>
         </header>
 
@@ -45,10 +34,6 @@ export default function AppLayout() {
           <Outlet />
         </main>
       </div>
-
-      {/* 오른쪽 사이드바 */}
-      {/* <RightSidebar isOpen={isRightSidebarOpen} onClose={toggleRightSidebar} /> */}
-
       {/* 토스트 알림 */}
       <ToastHost />
     </div>
--- mes_app/src/pages/equipstatus/EquipStatusPage.tsx
+++ mes_app/src/pages/equipstatus/EquipStatusPage.tsx
@@ -45,7 +45,7 @@ export default function EquipStatusPage() {
   });
 
   return (
-    <div className="max-w-5xl mx-auto p-4">
+    <div className="w-full p-4">
       <h1 className="text-2xl font-bold mb-5 text-black-700">설비 상태</h1>
 
          {/* 👉 사이드바 열기 버튼 */}
